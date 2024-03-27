a42f739a5a1d4b3b5bfe70238323bba3c6ed5bca
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-03-27 23:58:13 +0530

    Add react-router-dom dependency and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Home from "./pages/Home";
+import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
