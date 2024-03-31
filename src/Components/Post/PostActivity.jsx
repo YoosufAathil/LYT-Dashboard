205ed85b885c11c62524a6484b9c0d18e4177a32
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-03-31 17:24:58 +0530

    Update HTML meta tags and styles in Barchart, DemoChart, PostActivity, and home.scss files

--- src/Components/Post/PostActivity.jsx
+++ src/Components/Post/PostActivity.jsx
@@ -1,37 +1,62 @@
 import "./postactivity.scss";
 import avatar1 from "../../Assets/img/avatar1.jpg";
-// import avatar2 from "../../Assets/img/avatar2.jpg";
-import eyeicon from "../../Assets/img/eyeicon.png";
-import spinner from "../../Assets/img/spinner.png";
+import avatar2 from "../../Assets/img/avatar2.jpg";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const rows = [
+  {
+    image: avatar1,
+    desc: "Success can be achieved by being bold enough...",
+    Date: "May 30",
+    Number: "1.1k",
+  },
+  {
+    image: avatar2,
+    desc: "Knowing what to do can never be okay, you have to...",
+    Date: "May 30",
+    Number: "3.1k",
+  },
+  // ...
+];
 const PostActivity = () => {
   return (
     <div className="postactivity">
       <div className="heading">
         <span> Post Activity</span>
       </div>
-      <div className="content">
-        {/* <table>
-          <tbody>
-            <tr>
-              <td className="1st">
-                <img src={avatar1} alt="" className="profile" />
-              </td>
-              <td className="2nd">
-                Success can be achieved by being bold enough...
-              </td>
-              <td className="3rd">May 30</td>
-              <td>
-                <img src={eyeicon} alt="" className="icon" />
-              </td>
-              <td className="4th">1.1k</td>
-              <td className="5th">
-                <img src={spinner} alt="" className="icon" />
-              </td>
-            </tr>
-          </tbody>
-        </table> */}
-      </div>
+      <TableContainer className="content" component={Paper}>
+        <Table sx={{ maxWidth: 500 }} aria-label="simple table">
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  <img src={row.image} alt={row.desc} width={45} height={45} />
+                </TableCell>
+                <TableCell className="desc" width={220}>
+                  {row.desc}
+                </TableCell>
+                <TableCell className="date">{row.Date}</TableCell>
+                <TableCell className="eyetext">
+                  <RemoveRedEyeIcon className="icon" />
+                  {row.Number}
+                </TableCell>
+                <TableCell className="more">
+                  {" "}
+                  <MoreHorizIcon />{" "}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
