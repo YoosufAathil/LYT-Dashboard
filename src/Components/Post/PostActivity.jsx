51aac18f1a4ce4078e87892ddf554df2aebc879d
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-03-31 14:47:46 +0530

    Add avatar1, eyeicon, and spinner images

--- src/Components/Post/PostActivity.jsx
+++ src/Components/Post/PostActivity.jsx
@@ -1,4 +1,8 @@
 import "./postactivity.scss";
+import avatar1 from "../../Assets/img/avatar1.jpg";
+// import avatar2 from "../../Assets/img/avatar2.jpg";
+import eyeicon from "../../Assets/img/eyeicon.png";
+import spinner from "../../Assets/img/spinner.png";
 
 const PostActivity = () => {
   return (
@@ -6,7 +10,28 @@ const PostActivity = () => {
       <div className="heading">
         <span> Post Activity</span>
       </div>
-      <div className="content">Table</div>
+      <div className="content">
+        {/* <table>
+          <tbody>
+            <tr>
+              <td className="1st">
+                <img src={avatar1} alt="" className="profile" />
+              </td>
+              <td className="2nd">
+                Success can be achieved by being bold enough...
+              </td>
+              <td className="3rd">May 30</td>
+              <td>
+                <img src={eyeicon} alt="" className="icon" />
+              </td>
+              <td className="4th">1.1k</td>
+              <td className="5th">
+                <img src={spinner} alt="" className="icon" />
+              </td>
+            </tr>
+          </tbody>
+        </table> */}
+      </div>
     </div>
   );
 };
